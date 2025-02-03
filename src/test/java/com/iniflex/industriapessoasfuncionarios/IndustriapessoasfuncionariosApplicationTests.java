86aabd8d450cb54f873afae289df5b7edc7e5fff
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2025-02-03 04:25:38 -0300

    primerio commit

---
+++ src/test/java/com/iniflex/industriapessoasfuncionarios/IndustriapessoasfuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iniflex.industriapessoasfuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndustriapessoasfuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
